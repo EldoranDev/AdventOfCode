9f25a076b0ef5adbd5d246a5ae40661645928bd7
Marcel Behrmann <info@eldoan.dev>
2020-12-02 06:17:30 +0100

    Day 2 Part 2

--- src/days/02-2.ts
+++ src/days/02-2.ts
@@ -1,3 +1,27 @@
-export default function (input: string[]) {
+import { string } from "yargs";
 
+export default function (input: string[]) {
+    
+    
+
+    const lines = input.filter((line) => {
+        const parts = line.split(' ')
+
+        const [ a, b ] = parts[0].split('-').map(n => Number(n));
+
+        const letter = parts[1].substr(0, 1);
+
+        const posA = parts[2].charAt(a-1);
+        const posB = parts[2].charAt(b-1);
+
+        return (
+            posA !== posB
+            && (
+                posA === letter
+                || posB === letter 
+            )
+        );
+    });
+
+    return lines.length;
 };
