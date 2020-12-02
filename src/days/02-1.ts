6baff936238071a5ae016dd8641f4c2f668f5745
Marcel Behrmann <info@eldoan.dev>
2020-12-02 06:11:14 +0100

    Day 2 part 1

---
+++ src/days/02-1.ts
@@ -0,0 +1,29 @@
+import { string } from "yargs";
+
+export default function (input: string[]) {
+    
+    
+
+    const lines = input.filter((line) => {
+        const parts = line.split(' ')
+
+        const [ min, max ] = parts[0].split('-').map(n => Number(n));
+
+        const letter = parts[1].substr(0, 1);
+
+        const letters: Record<string, number> =  {};
+
+        for (let i = 0; i < parts[2].length; i++) {
+            const char = parts[2].charAt(i);
+
+            if (letters[char] === undefined) {
+                letters[char] = 0;
+            }
+
+            letters[char]++;
+        }
+        return letters[letter] >= min && letters[letter] <= max;
+    });
+
+    return lines.length;
+};
