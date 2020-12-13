8e1814f2a4d694e0b264ee26db95a0a7892b89a9
Marcel Behrmann <info@eldoran.dev>
2020-12-13 08:03:47 +0100

    Day 13 Part 1/2

---
+++ src/days/13-1.ts
@@ -0,0 +1,26 @@
+import { } from '../lib/input';
+
+export default function (input: string[]) {
+    let target = Number(input[0]);
+
+    let lowest = {
+        id: null,
+        time: null,
+    };
+
+    console.log(target);
+    for (let bus of input[1].split(',').map((b) => Number(b))) {
+        if (Number.isNaN(bus)) continue;
+
+        let diff = target % bus;
+        let depart = target - diff + bus;
+        let waiting = depart-target;
+
+        if (lowest.time === null || lowest.time > waiting) {
+            lowest.time = waiting;
+            lowest.id = bus;
+        }
+    }
+
+    return lowest.time * lowest.id;
+};
