52a20682be98b8c111af3e4fa4987eca33ee2e5a
Marcel Behrmann <info@eldoran.dev>
2020-12-17 09:09:07 +0100

    [lib] Add imports for tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+const tsconfig = require('./tsconfig.json');
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig);
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper,
 };
--- tests/lib/math/vec3.spec.ts
+++ tests/lib/math/vec3.spec.ts
@@ -1,20 +1,22 @@
-import Vec2 from '@lib/math/vec2';
+import Vec3 from '@lib/math/vec3';
 
 describe('Vec2', () => {
     it('should init as Zero', () => {
-        const v = new Vec2();
+        const v = new Vec3();
 
         expect(v.x).toBe(0);
         expect(v.y).toBe(0);
+        expect(v.z).toBe(0);
     });
 
     it ('should add component wise', () => {
-        const a = new Vec2(1, 2);
-        const b = new Vec2(3, 4);
+        const a = new Vec3(1, 2, 3);
+        const b = new Vec3(3, 4, 5);
 
-        const res = Vec2.add(a, b);
+        const res = Vec3.add(a, b);
 
         expect(res.x).toBe(4);
         expect(res.y).toBe(6);
+        expect(res.z).toBe(8);
     });
 });
