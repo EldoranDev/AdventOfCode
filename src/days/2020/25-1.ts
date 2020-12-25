c833faa923ec5bba548a3568270620c73708179e
Marcel Behrmann <info@eldoran.dev>
2020-12-25 06:19:53 +0100

    [2020] Day 25 Cleanup

--- src/days/2020/25-1.ts
+++ src/days/2020/25-1.ts
@@ -5,9 +5,7 @@ export default function (input: string[]) {
     let cardKey = Number(input[0]);
     let doorKey = Number(input[1]);
 
-
     let cardLoop = findLoopsize(7, cardKey);
-    // let doorLoop = findLoopsize(11, doorKey);
 
     let val = 1;
     let sub = doorKey;
