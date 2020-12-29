022edc09ea8dc22a809525d5d84731dfcdde09df
Marcel Behrmann <info@eldoran.dev>
2020-12-29 16:52:03 +0100

    [2015] Day 4 Part 1/2

---
+++ src/days/2015/04-1.ts
@@ -0,0 +1,16 @@
+import { createHash } from 'crypto';
+
+import { } from '@lib/input';
+
+export default function (input: string[]) {
+    const key = input[0];
+
+    for (let i = 1;; i++) {
+        const out = createHash('md5').update(key + i.toString()).digest('hex');
+
+        if (out.startsWith('00000')) {
+            console.log(out);
+            return i;
+        }
+    }
+};
