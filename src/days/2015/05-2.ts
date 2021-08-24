27ab27689d7863aa5b80cc0c77e888afe22d41f5
Marcel Behrmann <marcel.behrmann@real-digital.de>
2021-08-24 21:22:53 +0200

    [2015] Day 05 Part 02 done

--- src/days/2015/05-2.ts
+++ src/days/2015/05-2.ts
@@ -1,9 +1,20 @@
 import { } from '@lib/input';
 
+const RULE1 = /(..).*\1/;
+const RULE2 = /(.).\1/;
+
 export default function (input: string[]) {
     return input.filter(s => isNice(s)).length;
 };
 
 function isNice(check: string): boolean {
+    if (!RULE1.test(check)) {
+        return false;
+    }
+
+    if (!RULE2.test(check)) {
+        return false;
+    }
     
+    return true;
 }
