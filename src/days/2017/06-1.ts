16da13fa86deeab326cd5eb534d1f09ec7e96d3e
Marcel Behrmann <info@eldoran.dev>
2021-09-28 22:48:40 +0200

    [2017] d6p1 d6p2

---
+++ src/days/2017/06-1.ts
@@ -0,0 +1,28 @@
+import { } from '@lib/input';
+
+export default function (input: string[]) {
+    const banks = input[0].split('	').map(n => Number(n));
+
+    let loop = false;
+
+    const set = new Set<string>();
+
+    do {
+        const highest = Math.max(...banks);
+        const index = banks.indexOf(highest);
+
+        banks[index] = 0;
+
+        for (let i = 0; i < highest; i++) {
+            banks[(index + i + 1) % banks.length]++;
+        }
+
+        const key = banks.join('-');
+
+        loop = set.has(key);
+
+        set.add(key);
+    } while (!loop);
+
+    return set.size + 1;
+};
---
+++ src/days/2017/06-2.ts
@@ -0,0 +1,28 @@
+import { } from '@lib/input';
+
+export default function (input: string[]) {
+    const banks = input[0].split('	').map(n => Number(n));
+
+    let loop = false;
+
+    const set: string[] = [];
+    let key = null;
+    do {
+        const highest = Math.max(...banks);
+        const index = banks.indexOf(highest);
+
+        banks[index] = 0;
+
+        for (let i = 0; i < highest; i++) {
+            banks[(index + i + 1) % banks.length]++;
+        }
+
+        key = banks.join('-');
+
+        loop = set.includes(key);
+
+        set.push(key);
+    } while (!loop);
+
+    return (set.length-1) - set.indexOf(key);
+};
