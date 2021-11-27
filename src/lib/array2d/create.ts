16af1707e1a74f0fd60d41ef9c65b0c9cf5eacfc
Marcel Behrmann <marcel.behrmann@real-digital.de>
2021-11-27 10:30:23 +0100

    lib(array2d): add initial value

--- src/lib/array2d/create.ts
+++ src/lib/array2d/create.ts
@@ -1,8 +1,12 @@
-export default function create<T>(x: number, y: number): T[][] {
+export default function create<T>(x: number, y: number, initial: T = undefined): T[][] {
     const array: T[][] = Array.from({ length: y });
 
     for (let row = 0; row < y; row++) {
         array[row] = Array.from({ length: x });
+
+        if (initial) {
+            array[row].fill(initial);
+        }
     }
 
     return array;
