39d24d0cced8fadb1271038afcab68a48ab05e99
Marcel Behrmann <marcel.behrmann@real-digital.de>
2021-11-27 11:10:40 +0100

    day(2019): solve 14-1

---
+++ src/days/2019/14-1.ts
@@ -0,0 +1,86 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+
+type Recipe = { 
+    input: { 
+        [resource: string]: number 
+    },
+    output: number,
+};
+
+
+export default function (input: string[], { logger }: Context) {
+    const recipies: { [resource: string]: Recipe } = {};
+    const production: {
+        [resource: string]: {
+            required: number,
+            current: number,
+        }
+    } = {};
+
+    for (const line of input) {
+        const parts = line.split('=>').map(p => p.trim());
+        
+        const inputLines = parts[0].split(',').map(p => p.trim());
+        const input = {};
+
+        for (let inp of inputLines) {
+            const parts = inp.split(' ');
+            input[parts[1]] = Number(parts[0]);
+        }
+
+        const outputParts = parts[1].split(' ');
+
+        recipies[outputParts[1]] = {
+            input,
+            output: Number(outputParts[0]),
+        };
+    }
+
+    for (let resource of Object.keys(recipies)) {
+        production[resource] = {
+            required: 0,
+            current: 0,
+        };
+    }
+
+    production['FUEL'].required = 1;
+    production['ORE'] = {
+        required: 0,
+        current: 0,
+    }
+
+    let satisfied = true;
+
+    do {
+        satisfied = true;
+
+        for (let resource of Object.keys(production)) {
+            if (resource === 'ORE') {
+                production['ORE'].current = production['ORE'].required;
+                continue;
+            }
+
+            let prod = production[resource];
+
+            if (prod.required > prod.current) {
+                
+                let recipe = recipies[resource];
+
+                let count = Math.ceil(
+                    (prod.required - prod.current)/recipe.output
+                );
+
+                prod.current += count * recipe.output;
+
+                for (let inpResoure of Object.keys(recipe.input)) {
+                    production[inpResoure].required += recipe.input[inpResoure] * count;    
+                }
+
+                satisfied = false;
+            }
+        }
+    } while (!satisfied);
+
+    return production['ORE'].required;
+};
