ae66c24ea5f338fa7ebb82d7994835ac25893f8a
Marcel Behrmann <marcel.behrmann@real-digital.de>
2021-12-03 06:22:54 +0100

    day(2021): solve day 3 p1+2

---
+++ src/days/2021/03-1.ts
@@ -0,0 +1,38 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+import { create } from '@lib/array2d';
+
+export default function (input: string[], { logger }: Context) {
+    let numbers: string[][] = create(0, input[0].length);
+    
+    
+    for (let line of input) {
+        for (let i = 0; i < line.length; i++) {
+            logger.debug(line.charAt(i));
+            numbers[i].push(line.charAt(i));
+        }
+    }
+
+    let gammaIn = "";
+    let epsilonIn = "";
+
+    for (let i = 0; i < numbers.length; i++) {
+        let ones = numbers[i].filter(n => n === "1").length;
+
+        if (ones > numbers[i].length / 2) {
+            epsilonIn += "0";
+            gammaIn += "1";
+        } else {
+            epsilonIn += "1";
+            gammaIn += "0";
+        }
+    }
+
+    logger.debug(epsilonIn);
+    logger.debug(gammaIn);
+    let gamma = parseInt(gammaIn, 2);
+    let epsilon = parseInt(epsilonIn, 2);
+
+    return gamma * epsilon;
+
+};
---
+++ src/days/2021/03-2.ts
@@ -0,0 +1,45 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+import { create } from '@lib/array2d';
+
+export default function (input: string[], { logger }: Context) {
+    let search = [...input ];
+    let position = 0;
+
+    while (search.length > 1) {
+        let ones = search.filter(l => l.charAt(position) === "1").length;
+        
+        if (ones >= search.length/2) {
+            logger.debug(`${ones} >= ${search.length}`);
+            search = search.filter(l => l.charAt(position) === "1");
+        } else {
+            search = search.filter(l => l.charAt(position) === "0")
+        }
+
+        position++;
+    }
+
+    let gen = parseInt(search[0], 2);
+
+    search = [...input ];
+    position = 0;
+
+    while (search.length > 1) {
+        let ones = search.filter(l => l.charAt(position) === "1").length;
+        
+        if (ones >= search.length/2) {
+            logger.debug(`${ones} > ${search.length}`);
+            search = search.filter(l => l.charAt(position) === "0");
+        } else {
+            search = search.filter(l => l.charAt(position) === "1")
+        }
+
+        position++;
+    }
+
+    let scrubber = parseInt(search[0], 2);
+
+    
+    return gen * scrubber;
+
+};
