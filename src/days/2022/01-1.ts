769637c22843207a44de99a8622326bd02c433ef
Marcel Behrmann <info@eldoran.dev>
2022-12-01 07:27:35 +0100

    solve(2022): Solving Day 1

---
+++ src/days/2022/01-1.ts
@@ -0,0 +1,13 @@
+import { getLineGroups } from '@lib/input';
+import { sum } from '@lib/math/functions';
+
+export default function (input: string[]) {
+    const elves = getLineGroups(input);
+    const calories: number[] = [];
+
+    for (let i = 0; i < elves.length; i++) {
+        calories[i] = sum(...elves[i].map((s) => Number(s)));
+    }
+
+    return calories.sort((a, b) => b - a)[0];
+}
---
+++ src/days/2022/01-2.ts
@@ -0,0 +1,22 @@
+import { getLineGroups } from '@lib/input';
+import { sum } from '@lib/math/functions';
+
+export default function (input: string[]) {
+    const elves = getLineGroups(input);
+
+    let calories: number[] = [];
+
+    for (let i = 0; i < elves.length; i++) {
+        calories[i] = sum(...elves[i].map((s) => Number(s)));
+    }
+
+    calories = calories.sort((a, b) => b - a);
+
+    let s = 0;
+
+    for (let i = 0; i < 3; i++) {
+        s += calories[i];
+    }
+
+    return s;
+}
