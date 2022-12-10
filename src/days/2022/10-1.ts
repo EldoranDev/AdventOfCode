bed0e6079ec9146c0a372077e2ab54b592707954
Marcel Behrmann <info@eldoran.dev>
2022-12-10 09:20:28 +0100

    solve(2022): Solving Day 10

---
+++ src/days/2022/10-1.ts
@@ -0,0 +1,36 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+
+const NOOP = 'noop';
+
+export default function (input: string[], { logger }: Context) {
+    let X = 1;
+
+    let BUFFER: number | null = null;
+
+    let strength: number = 0;
+
+    let OP = 0;
+
+    const PTS = [20, 60, 100, 140, 180, 220];
+
+    for (let i = 0; OP < input.length; i++) {
+        if (PTS.includes(i + 1)) {
+            console.log(X, i + 1);
+            strength += X * (i + 1);
+        }
+
+        if (BUFFER !== null) {
+            X += BUFFER;
+            BUFFER = null;
+            OP++;
+        } else if (input[OP] === NOOP) {
+            OP++;
+        } else {
+            const [, amount] = input[OP].split(' ');
+            BUFFER = Number(amount);
+        }
+    }
+
+    return strength;
+}
---
+++ src/days/2022/10-2.ts
@@ -0,0 +1,37 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+import { create } from '@lib/array2d';
+
+const NOOP = 'noop';
+
+export default function (input: string[], { logger }: Context) {
+    let X = 1;
+    let CRT_POS = 0;
+
+    let BUFFER: number | null = null;
+
+    let OP = 0;
+
+    const CRT = create(40, 6, '.');
+
+    for (let i = 0; OP < input.length; i++) {
+        if (CRT_POS % 40 >= X - 1 && CRT_POS % 40 <= X + 1) {
+            CRT[(CRT_POS / 40) | 0][CRT_POS % 40] = '#';
+        }
+
+        if (BUFFER !== null) {
+            X += BUFFER;
+            BUFFER = null;
+            OP++;
+        } else if (input[OP] === NOOP) {
+            OP++;
+        } else {
+            const [, amount] = input[OP].split(' ');
+            BUFFER = Number(amount);
+        }
+
+        CRT_POS++;
+    }
+
+    return CRT.reduce((prev, line) => `${prev}\n${line.join('')}`, '');
+}
