05a8ea53037038c1d0bfb3a3e24ff4fad898c758
Marcel Behrmann <info@eldoran.dev>
2023-01-01 19:38:17 +0100

    solve(2015): solving day 8

--- src/days/2015/08-1.ts
+++ src/days/2015/08-1.ts
@@ -4,11 +4,62 @@ export default function (input: string[]) {
     let length = 0;
 
     for (let line of input) {
-        var a = '';
-
-        eval(`a = ${line}`);
-
-        length += line.length - a.length;
+        try {
+            let a = getString(line);
+    
+            length += line.length - a.length;
+        } catch (e) {
+            console.log(e);
+            console.log(line);
+        }
     }
     return length;
 };
+
+function getString(db: string): string {
+    let res = '';
+
+    for (let i = 0; i < db.length;)  {
+        let current = getToken(db, i);
+        
+        i += current.length;
+
+        res += toString(current);
+    }
+
+    return res;
+}
+
+function toString(token: string): string {
+    if (token === '"') {
+        return '';
+    }
+
+    if (token.substring(0, 2) === '\\x') {
+        return String.fromCharCode(Number(token.substring(2)));
+    }
+
+    if (token[0] === '\\') {
+        return token[1];
+    }
+
+    return token;
+}
+
+const LITS = ['"', '\'', '\\'];
+
+function getToken(str: string, position: number): string {
+    if (str[position] === '\\') {
+        if (LITS.includes(str[position + 1])) {
+            return str.substring(position, position + 2);
+        }
+
+        if (str[position + 1] === 'x') {
+            return str.substring(position, position + 4);
+        }
+
+        throw new Error('Invalid Escape');
+    }
+
+    return str[position];
+}
--- src/days/2015/08-2.ts
+++ src/days/2015/08-2.ts
@@ -4,19 +4,60 @@ export default function (input: string[]) {
     let length = 0;
 
     for (let line of input) {
-        var a = '';
-        let escape = addEscape(line);
-        eval(`a = ${line}`);
+        try {
+            let a = getString(line);
+    
+            console.log(a);
 
-        length += escape.length - line.length;
+            length += a.length - line.length;
+        } catch (e) {
+            console.log(e);
+            console.log(line);
+        }
     }
-
     return length;
 };
 
-function addEscape(line: string): string {
-    line = line.split('\\').join('\\\\');
-    line = line.split('\\\\"').join('\\\\\\"');
-    
-    return `"\\"${line.substr(1, line.length-2)}\\""`;
+function getString(db: string): string {
+    let res = '';
+
+    for (let i = 0; i < db.length;)  {
+        let current = getToken(db, i);
+        
+        i += current.length;
+
+        res += escape(current);
+    }
+
+    return `"${res}"`;
+}
+
+function escape(token: string): string {
+    if (token === '"') {
+        return `\\"`;
+    }
+
+    if (token.length > 0 && token[1] === '"') {
+        return '\\\\\\"';
+    }
+
+    return token.replaceAll('\\', '\\\\');
+}
+
+const LITS = ['"', '\'', '\\'];
+
+function getToken(str: string, position: number): string {
+    if (str[position] === '\\') {
+        if (LITS.includes(str[position + 1])) {
+            return str.substring(position, position + 2);
+        }
+
+        if (str[position + 1] === 'x') {
+            return str.substring(position, position + 4);
+        }
+
+        throw new Error('Invalid Escape');
+    }
+
+    return str[position];
 }
