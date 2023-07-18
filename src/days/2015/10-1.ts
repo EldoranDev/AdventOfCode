f6c45eed4540466ccb61493e3793c733c9393b4e
Marcel Behrmann <info@eldoran.dev>
2023-07-18 11:20:25 +0200

    solve(2015): Day 10 Part 1/2

---
+++ src/days/2015/10-1.ts
@@ -0,0 +1,36 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+
+export default function (input: string[], { logger }: Context) {
+    let line = input[0];
+
+    for (let i = 0; i < 40; i++) {
+        line = transform(line);
+    }
+
+    return line.length;
+}
+
+type Entry = { num: string, count: number };
+
+function transform(value: string): string {
+    const groups: Array<Entry> = [];
+
+    groups.push({ num: value[0], count: 0 });
+
+    for (const char of value.split('')) {
+        if (groups[groups.length - 1].num === char) {
+            groups[groups.length - 1].count++;
+        } else {
+            groups.push({ num: char, count: 1 });
+        }
+    }
+
+    let ouput: string = '';
+
+    for (const group of groups) {
+        ouput += `${group.count}${group.num}`;
+    }
+
+    return ouput;
+}
