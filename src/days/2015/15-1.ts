40d0163735e3db0f96d70f1b035c4a0a5e4c18bc
Marcel Behrmann <info@eldoran.dev>
2023-07-19 13:09:09 +0200

    solve(2015): Day 15 part 1/2

---
+++ src/days/2015/15-1.ts
@@ -0,0 +1,65 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+
+type Property = {
+    capacity: number;
+    durability: number;
+    flavor: number;
+    texture: number;
+    calories: number;
+};
+
+const INGREDIENTS: Array<Property> = [];
+
+const EXTRACTOR = /(\w+): capacity (-?\d), durability (-?\d), flavor (-?\d). texture (-?\d), calories (-?\d)/;
+
+let highest = 0;
+
+export default function (input: string[], { logger }: Context) {
+    for (const line of input) {
+        const [, name, capacity, durability, flavor, texture, calories] = EXTRACTOR.exec(line)!;
+        INGREDIENTS.push({
+            capacity: Number(capacity),
+            durability: Number(durability),
+            flavor: Number(flavor),
+            texture: Number(texture),
+            calories: Number(calories),
+        });
+    }
+
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
+
+    for (a = 0; a + b + c + d <= 100; a++) {
+        for (b = 0; a + b + c + d <= 100; b++) {
+            for (c = 0; a + b + c + d <= 100; c++) {
+                for (d = 0; a + b + c + d <= 100; d++) {
+                    const cap = Math.max(
+                        0,
+                        // eslint-disable-next-line max-len
+                        a * INGREDIENTS[0].capacity + b * INGREDIENTS[1].capacity + c * INGREDIENTS[2].capacity + d * INGREDIENTS[3].capacity,
+                    );
+                    // eslint-disable-next-line max-len
+                    const dur = Math.max(0, a * INGREDIENTS[0].durability + b * INGREDIENTS[1].durability + c * INGREDIENTS[2].durability + d * INGREDIENTS[3].durability);
+                    // eslint-disable-next-line max-len
+                    const fla = Math.max(0, a * INGREDIENTS[0].flavor + b * INGREDIENTS[1].flavor + c * INGREDIENTS[2].flavor + d * INGREDIENTS[3].flavor);
+                    // eslint-disable-next-line max-len
+                    const tex = Math.max(0, a * INGREDIENTS[0].texture + b * INGREDIENTS[1].texture + c * INGREDIENTS[2].texture + d * INGREDIENTS[3].texture);
+
+                    const score = cap * dur * fla * tex;
+
+                    if (score > highest) {
+                        highest = score;
+                    }
+                }
+                d = 0;
+            }
+            c = 0;
+        }
+        b = 0;
+    }
+
+    return highest;
+}
---
+++ src/days/2015/15-2.ts
@@ -0,0 +1,89 @@
+import { } from '@lib/input';
+import { Context } from '@app/types';
+import { sum } from '@lib/math/functions';
+
+type Property = {
+    capacity: number;
+    durability: number;
+    flavor: number;
+    texture: number;
+    calories: number;
+};
+
+const INGREDIENTS: Array<Property> = [];
+
+const EXTRACTOR = /(\w+): capacity (-?\d), durability (-?\d), flavor (-?\d). texture (-?\d), calories (-?\d)/;
+
+const CACHE = new Map<string, number>();
+
+export default function (input: string[], { logger }: Context) {
+    for (const line of input) {
+        const [, name, capacity, durability, flavor, texture, calories] = EXTRACTOR.exec(line)!;
+        INGREDIENTS.push({
+            capacity: Number(capacity),
+            durability: Number(durability),
+            flavor: Number(flavor),
+            texture: Number(texture),
+            calories: Number(calories),
+        });
+    }
+
+    return addIngredient(Array.from({ length: INGREDIENTS.length }, () => 0));
+}
+
+function addIngredient(ingredients: Array<number>): number {
+    const cacheKey = ingredients.join(',');
+
+    if (sum(...ingredients) === 100) {
+        let capacity = 0;
+        let durability = 0;
+        let flavor = 0;
+        let texture = 0;
+        let calories = 0;
+
+        for (let i = 0; i < ingredients.length; i++) {
+            capacity += ingredients[i] * INGREDIENTS[i].capacity;
+            durability += ingredients[i] * INGREDIENTS[i].durability;
+            flavor += ingredients[i] * INGREDIENTS[i].flavor;
+            texture += ingredients[i] * INGREDIENTS[i].texture;
+            calories += ingredients[i] * INGREDIENTS[i].calories;
+        }
+
+        capacity = Math.max(0, capacity);
+        durability = Math.max(0, durability);
+        flavor = Math.max(0, flavor);
+        texture = Math.max(0, texture);
+
+        let score = capacity * durability * flavor * texture;
+
+        if (calories !== 500) {
+            score = 0;
+        }
+
+        CACHE.set(cacheKey, score);
+
+        return score;
+    }
+
+    if (CACHE.has(cacheKey)) {
+        return CACHE.get(cacheKey)!;
+    }
+
+    const options = [];
+
+    for (let i = 0; i < ingredients.length; i++) {
+        const newIngredients = [...ingredients];
+        newIngredients[i]++;
+
+        options.push(addIngredient(newIngredients));
+    }
+
+    const highest = Math.max(...options);
+
+    CACHE.set(cacheKey, highest);
+
+    return highest;
+}
+
+// 62842880
+// 21367368
