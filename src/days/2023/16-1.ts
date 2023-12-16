2cf0b4b1aea301d034a46e4e5a3cd2de09ecd4a9
Marcel Behrmann <info@eldoran.dev>
2023-12-16 12:57:14 +0100

    solve(2023): Day 16

---
+++ src/days/2023/16-1.ts
@@ -0,0 +1,146 @@
+/* eslint-disable max-len */
+import { } from '@lib/input';
+import { Context } from '@app/types';
+import { Grid2D, create } from '@lib/array2d';
+import { Vec2 } from '@lib/math';
+
+type Tile = '.' | '/' | '\\' | '-' | '|';
+
+interface Field {
+    type: Tile;
+    energy: number;
+}
+
+interface Beam {
+    pos: Vec2;
+    direction: Vec2;
+}
+
+export default function (input: string[], { logger }: Context) {
+    const grid: Grid2D<Field> = create(input[0].length, input.length, null);
+
+    for (let y = 0; y < input.length; y++) {
+        for (let x = 0; x < input[y].length; x++) {
+            grid[y][x] = {
+                type: input[y][x] as Field['type'],
+                energy: 0,
+            };
+        }
+    }
+
+    let beams: Array<Beam> = [
+        {
+            pos: new Vec2(-1, 0),
+            direction: new Vec2(1, 0),
+        },
+    ];
+
+    while (beams.length > 0) {
+        const newBeams: Array<Beam> = [];
+
+        for (const beam of beams) {
+            beam.pos.add(beam.direction);
+
+            if (
+                beam.pos.y < 0
+                || beam.pos.y >= grid.length
+                || beam.pos.x < 0
+                || beam.pos.x >= grid[beam.pos.y].length
+            ) {
+                // Remove beam from list as we do not need to process it anymore
+                continue;
+            }
+
+            grid[beam.pos.y][beam.pos.x].energy++;
+
+            if (grid[beam.pos.y][beam.pos.x].energy > 1 && (grid[beam.pos.y][beam.pos.x].type === '|' || grid[beam.pos.y][beam.pos.x].type === '-')) {
+                continue;
+            }
+
+            newBeams.push(...getBeams(beam, grid[beam.pos.y][beam.pos.x].type));
+        }
+
+        beams = newBeams;
+    }
+
+    let totalEnergy = 0;
+
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            totalEnergy += grid[y][x].energy > 0 ? 1 : 0;
+        }
+    }
+
+    return totalEnergy;
+}
+
+function getBeams(beam: Beam, tile: Tile): Array<Beam> {
+    switch (tile) {
+        case '.':
+            return [beam];
+        case '|':
+            if (beam.direction.x === 0) {
+                return [beam];
+            }
+
+            return [
+                {
+                    pos: new Vec2(beam.pos.x, beam.pos.y),
+                    direction: new Vec2(0, 1),
+                },
+                {
+                    pos: new Vec2(beam.pos.x, beam.pos.y),
+                    direction: new Vec2(0, -1),
+                },
+            ];
+        case '-':
+            if (beam.direction.y === 0) {
+                return [beam];
+            }
+
+            return [
+                {
+                    pos: new Vec2(beam.pos.x, beam.pos.y),
+                    direction: new Vec2(1, 0),
+                },
+                {
+                    pos: new Vec2(beam.pos.x, beam.pos.y),
+                    direction: new Vec2(-1, 0),
+                },
+            ];
+        case '/':
+            if (beam.direction.x !== 0) {
+                return [
+                    {
+                        pos: new Vec2(beam.pos.x, beam.pos.y),
+                        direction: new Vec2(0, -beam.direction.x),
+                    },
+                ];
+            }
+
+            return [
+                {
+                    pos: new Vec2(beam.pos.x, beam.pos.y),
+                    direction: new Vec2(-beam.direction.y, 0),
+                },
+            ];
+        case '\\':
+            if (beam.direction.x !== 0) {
+                return [
+                    {
+                        pos: new Vec2(beam.pos.x, beam.pos.y),
+                        direction: new Vec2(0, beam.direction.x),
+                    },
+                ];
+            }
+
+            return [
+                {
+                    pos: new Vec2(beam.pos.x, beam.pos.y),
+                    direction: new Vec2(beam.direction.y, 0),
+                },
+            ];
+        default:
+            throw new Error(`Unknown tile ${tile}`);
+    }
+}
